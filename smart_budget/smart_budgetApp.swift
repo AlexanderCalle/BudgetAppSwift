c7b2aedfc3475901c762d185e6d980d3cf598aba
AlexanderCalle <alexander.callebaut2@student.hogent.be>
2024-10-06 16:06:54 +0200

    Initial Commit

---
+++ smart_budget/smart_budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_budgetApp.swift
+//  smart_budget
+//
+//  Created by Alexander Callebaut on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct smart_budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
