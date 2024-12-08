e66680471d6f546b18230be5b30a96a43fa2f08d
AlexanderCalle <alexander.callebaut2@student.hogent.be>
2024-12-08 20:35:26 +0100

    Refresh token refactoring for one request and retry the failed requests

---
+++ smart_budget/Models/AuthErrorResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AuthErrorResponse.swift
+//  smart_budget
+//
+//  Created by Alexander Callebaut on 07/12/2024.
+//
+
+import Foundation
---
+++ smart_budget/Utilities/SyncRefreshInterceptor.swift
@@ -0,0 +1,8 @@
+//
+//  SyncRefreshInterceptor.swift
+//  smart_budget
+//
+//  Created by Alexander Callebaut on 08/12/2024.
+//
+
+import Foundation
